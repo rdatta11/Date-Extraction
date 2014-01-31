5c247f5cd2182a9eaf5f2c92b950df0f8384260e
rdatta11 <rdatta11@gmail.com>
2014-01-31 15:57:35 -0600

    Support for months up till september

--- date.py
+++ date.py
@@ -5,5 +5,5 @@ import sys
 textFile = open(sys.argv[1], 'r')
 fileText = textFile.read()
 textFile.close()
-matches = re.findall("((?:jan|january|feb(?:(?:.)?|(?:ruary)?)|march|apr|april|may|jun|june|july|jul|aug|august|sep|sept|september|oct|october|nov|november|dec|december) (?:[12][0-9]|[1-9]))",fileText,re.IGNORECASE)
+matches = re.findall("((?:jan(?:(?:.)?|(?:uary)?)|feb(?:(?:.)?|(?:ruary)?)|mar(?:(?:.)?|(?:ch)?)|apr(?:(?:.)?|(?:il)?)|may|jun(?:(?:.)?|(?:e)?)|jul(?:(?:.)?|(?:y)?)|aug(?:(?:.)?|(?:gust)?)|sep|sept|september|oct|october|nov|november|dec|december) (?:[12][0-9]|[1-9]))",fileText,re.IGNORECASE)
 print matches
