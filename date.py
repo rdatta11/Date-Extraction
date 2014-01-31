0dffce7426a545d39722c1669d4217a0562b4314
rdatta11 <rdatta11@gmail.com>
2014-01-31 15:41:21 -0600

    Initial Commit
    Basic month with date support

---
+++ date.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import re
+import sys
+
+textFile = open(sys.argv[1], 'r')
+fileText = textFile.read()
+textFile.close()
+matches = re.findall("((?:jan|january|feb|february|march|apr|april|may|jun|june|july|jul|aug|august|sep|sept|september|oct|october|nov|november|dec|december) (?:[12][0-9]|[1-9]))",fileText,re.IGNORECASE)
+print matches
