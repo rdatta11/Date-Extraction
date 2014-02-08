17d037a54c47f4df3fafa0355dafae668e5ed9dc
rdatta11 <rdatta11@gmail.com>
2014-02-07 22:42:18 -0600

    doesn't recognize "day th" anymore

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-Date-Extraction
+Homework 1: Date-Extraction-- Leveraging NLP and Regex to extract date information from files
 ===============
+---
 
-Leveraging NLP and Regex to extract date information from files
+**Overview:**
+
+In this assignment the goal was to extract date information from a text file using basic Natural Language Processing techniques (NLP) including Regular Expressions (regex). Because of the various ways to represent dates it was difficult to cover all possible date combinations. I did my best to include the most common date formats.
+
+**To Run:**
+
+1. Install Python 2.7.6 on your UNIX based system.
+2. Change the permissions on the date.py file by typing in the command line: `$: chmod a+x date.py`.
+3. Execute date.py with an input test file as a command line argument. E.g.: `$: ./date.py input.txt`.
+4. The output will appear in the console.
+
+**Interpreting Output:**
+
+Upon executing date.py there will be five different sets with dates (each set represents a different style of date formatting). Each date in a set appears in the order in which it appears in the file relative to the other dates in the set.
+
+Please note that because of the complexities of English it is possible to get false positives (E.g. Handling the case 'mon the 21st' may result in finding the 'mon' in the word 'common'). It is also possible to get false negatives as it is not possible to cover every case for dates   
--- date.py
+++ date.py
@@ -2,16 +2,16 @@
 import re
 import sys
 
-textFile = open(sys.argv[1], 'r')
-fileText = textFile.read()
-textFile.close()
-matches = re.findall("(?:(?:jan(?:(?:.)?|(?:uary)?)|feb(?:(?:.)?|(?:ruary)?)|mar(?:(?:.)?|(?:ch)?)|apr(?:(?:.)?|(?:il)?)|may|jun(?:(?:.)?|(?:e)?)|jul(?:(?:.)?|(?:y)?)|aug(?:(?:.)?|(?:gust)?)|sep(?:(?:.)?|(?:ept(?:(?:.)?))?|(?:tember)?)|oct(?:(?:.)?|(?:ober)?)|nov(?:(?:.)?|(?:ember)?)|dec(?:(?:.)?|(?:ember)?)) (?:[123][0-9]|[1-9])[ \t\r\f\v]?(?:rd|st|th)?(?:,)?[ \t\r\f\v]?(?:[0-2][0-9][0-9][0-9])?)",fileText,re.IGNORECASE)
-print matches
-matches = re.findall("(?:(?:[0]?[1-9])|(?:[1][0-2]))[-/](?:(?:[012]?[0-9])|(?:[3][01]))[/-][12]?[0-9]?[0-9][0-9]",fileText, re.IGNORECASE)
-print matches
-matches = re.findall("(?:christmas|memorial day|labor day|halloween|new years eve|new year's eve|mothers day|mother's day|martin luther king day|presidents day|president's day|memorial day| independence day|labor day|columbus day|veterans day|valentines day|valentine's day|halloween|st. patricks day|st. patricks day|veteran's day|thanksgiving|thanksgiving day)",fileText, re.IGNORECASE)
-print matches
-matches = re.findall("(?:mon(?:\.|day)?|tue(?:\.|sday)?|wed(?:\.|nesday)?|thur(?:\.|sday)?|fri(?:\.|day)?|sat(?:\.|urday)?|sun(?:\.|day)?)?[ \t\r\f\v]?the (?:[123][0-9]|[1-9])?[ \t\r\f\v]?(?:rd|st|th)?(?:,)?[ \t\r\f\v]?[ \t\r\f\v]?of?[ \t\r\f\v]?(?:jan(?:\.|uary)?|feb(?:\.|ruary)?|mar(?:\.|ch)?|apr(?:\.|il)?|may|jun(?:\.|e)?|jul(?:\.|y)?|aug(?:\.|ust)?|oct(?:\.|ober)?|nov(?:\.|ember)?|dec(?:\.|ember)?),?[ \t\r\f\v]?(?:[0-2][0-9][0-9][0-9])?",fileText, re.IGNORECASE)
-print matches
-matches = re.findall("(?:mon(?:\.|day)?|tue(?:\.|sday)?|wed(?:\.|nesday)?|thur(?:\.|sday)?|fri(?:\.|day)?|sat(?:\.|urday)?|sun(?:\.|day)?)[ \t\r\f\v]?[t]?[h]?[e]?[ \t\f\r\v]?(?:[123][0-9]|[1-9])?[ \t\r\f\v]?(?:rd|st|th)?",fileText, re.IGNORECASE)
-print matches
+textFile = open(sys.argv[1], 'r') #open in file name from terminal
+fileText = textFile.read() #read the opened file
+textFile.close() #close the file
+matches = re.findall("(?:(?:jan(?:(?:.)?|(?:uary)?)|feb(?:(?:.)?|(?:ruary)?)|mar(?:(?:.)?|(?:ch)?)|apr(?:(?:.)?|(?:il)?)|may|jun(?:(?:.)?|(?:e)?)|jul(?:(?:.)?|(?:y)?)|aug(?:(?:.)?|(?:gust)?)|sep(?:(?:.)?|(?:ept(?:(?:.)?))?|(?:tember)?)|oct(?:(?:.)?|(?:ober)?)|nov(?:(?:.)?|(?:ember)?)|dec(?:(?:.)?|(?:ember)?)) (?:[123][0-9]|[1-9])[ \t\r\f\v]?(?:rd|st|th)?(?:,)?[ \t\r\f\v]?(?:[0-2][0-9][0-9][0-9])?)",fileText,re.IGNORECASE) #find all instances of dates like february 10th, 2014
+print matches #print findings
+matches = re.findall("(?:(?:[0]?[1-9])|(?:[1][0-2]))[-/](?:(?:[012]?[0-9])|(?:[3][01]))[/-][12]?[0-9]?[0-9][0-9]",fileText, re.IGNORECASE) #find all cases of American style date i.e. 10/12/98
+print matches #print finding
+matches = re.findall("(?:christmas|memorial day|labor day|halloween|new years eve|new year's eve|mothers day|mother's day|martin luther king day|presidents day|president's day|memorial day| independence day|labor day|columbus day|veterans day|valentines day|valentine's day|halloween|st. patricks day|st. patricks day|veteran's day|thanksgiving|thanksgiving day)",fileText, re.IGNORECASE) #find all instances of important holidays
+print matches #print findings
+matches = re.findall("(?:mon(?:\.|day)?|tue(?:\.|sday)?|wed(?:\.|nesday)?|thur(?:\.|sday)?|fri(?:\.|day)?|sat(?:\.|urday)?|sun(?:\.|day)?)?[ \t\r\f\v]?the (?:[123][0-9]|[1-9])?[ \t\r\f\v]?(?:rd|st|th)?(?:,)?[ \t\r\f\v]?[ \t\r\f\v]?of?[ \t\r\f\v]?(?:jan(?:\.|uary)?|feb(?:\.|ruary)?|mar(?:\.|ch)?|apr(?:\.|il)?|may|jun(?:\.|e)?|jul(?:\.|y)?|aug(?:\.|ust)?|oct(?:\.|ober)?|nov(?:\.|ember)?|dec(?:\.|ember)?),?[ \t\r\f\v]?(?:[0-2][0-9][0-9][0-9])?",fileText, re.IGNORECASE) #find instances of dates formatted like "the 21st of december 2014"
+print matches #print findings
+matches = re.findall("(?:mon(?:|day)?|tue(?:|sday)?|wed(?:|nesday)?|thur(?:|sday)?|fri(?:|day)?|sat(?:|urday)?|sun(?:|day)?)[ \t\r\f\v](?:the)?[ \t\f\r\v]?(?:(?:[123][0-9]|[1-9])[ \t\r\f\v]?(?:rd|st|th)?)?",fileText, re.IGNORECASE) # find instances of dates formatted like "Monday the 23rd"
+print matches #print findings
